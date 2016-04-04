aedb52a5fe67ba74a626a9b300258e1286b0cb3b
Yury Korolev <yury.korolev@gmail.com>
2016-04-04 14:06:52 +0300

    Code cleanup

--- Pod/Classes/SwiftURL.swift
+++ Pod/Classes/SwiftURL.swift
@@ -1,18 +1,26 @@
 import Foundation
 
 // Creates URLPathSegmentAllowedCharacterSet same as URLPathAllowedCharacterSet - "/"
-private func _buildURLPathSegmentAllowedCharacterSet() -> NSCharacterSet {
-    let pathSegmentCharacterSet = NSCharacterSet.URLPathAllowedCharacterSet().mutableCopy() as! NSMutableCharacterSet
+private func _createURLPathSegmentAllowedCharacterSet() -> NSCharacterSet {
+    let pathSegmentCharacterSet = NSCharacterSet
+        .URLPathAllowedCharacterSet()
+        .mutableCopy() as! NSMutableCharacterSet
+    
     pathSegmentCharacterSet.removeCharactersInString("/")
+    
     return pathSegmentCharacterSet
 }
 
 // Global var with URLPathSegmentAllowedCharacterSet to reduce
-private let _URLPathSegmentAllowedCharacterSet = _buildURLPathSegmentAllowedCharacterSet()
+private let _URLPathSegmentAllowedCharacterSet = _createURLPathSegmentAllowedCharacterSet()
 
 private func _pathSegmentsToPath(segments: [AnyObject]?) -> String? {
     guard let segments = segments else { return nil }
-    return segments.map { $0.description.stringByAddingPercentEncodingWithAllowedCharacters(_URLPathSegmentAllowedCharacterSet) ?? $0.description }.joinWithSeparator("/")
+    
+    return segments.map {
+        $0.description
+            .stringByAddingPercentEncodingWithAllowedCharacters(_URLPathSegmentAllowedCharacterSet) ?? $0.description
+        }.joinWithSeparator("/")
 }
 
 // Encode complex key/value objects in NSRULQueryItem pairs
@@ -24,7 +32,7 @@ private func _queryItems(key: String, _ value: AnyObject?) -> [NSURLQueryItem] {
             result += _queryItems("\(key)[\(nestedKey)]", value)
         }
     } else if let array = value as? [AnyObject] {
-        let arrKey = "\(key)[]"
+        let arrKey = key + "[]"
         for value in array {
             result += _queryItems(arrKey, value)
         }
@@ -52,46 +60,53 @@ private func _paramsToQueryItems(params: [String: AnyObject]?) -> [NSURLQueryIte
 
 public extension NSURLComponents {
     
-    // path as String
+    // MARK: path as String
+    
     @nonobjc
     convenience init(path: String, query: String?, fragment: String? = nil) {
         self.init()
-        self.path = path
-        self.query = query
+        
+        self.path     = path
+        self.query    = query
         self.fragment = fragment
     }
     
     @nonobjc
     convenience init(path: String, queryItems: [NSURLQueryItem]?, fragment: String? = nil) {
         self.init()
-        self.path = path
+        
+        self.path       = path
         self.queryItems = queryItems
-        self.fragment = fragment
+        self.fragment   = fragment
     }
     
     @nonobjc
     convenience init(path: String, query: [String: AnyObject]?, fragment: String? = nil) {
         self.init()
-        self.path = path
+        
+        self.path       = path
         self.queryItems = _paramsToQueryItems(query)
-        self.fragment = fragment
+        self.fragment   = fragment
     }
    
-    // path as array of segments
+    // MARK: path as array of segments
+    
     @nonobjc
     convenience init(path segments: [AnyObject]?, query: String?, fragment: String? = nil) {
         self.init()
+        
         self.percentEncodedPath = _pathSegmentsToPath(segments)
-        self.query = query
-        self.fragment = fragment
+        self.query              = query
+        self.fragment           = fragment
     }
    
     @nonobjc
     convenience init(path segments: [AnyObject]?, query: [String: AnyObject]?, fragment: String? = nil) {
         self.init()
+        
         self.percentEncodedPath = _pathSegmentsToPath(segments)
-        self.queryItems = _paramsToQueryItems(query)
-        self.fragment = fragment
+        self.queryItems         = _paramsToQueryItems(query)
+        self.fragment           = fragment
     }
 }
 
@@ -113,7 +128,7 @@ public extension NSURL {
     }
     
     @nonobjc
-    func build(path: String, query: String, fragment: String? = nil) -> NSURL? {
+    final func build(path: String, query: String, fragment: String? = nil) -> NSURL? {
         return build(NSURLComponents(path: path, query: query, fragment: fragment))
     }
     
@@ -149,20 +164,25 @@ public extension NSURL {
     
     @nonobjc
     static func build(scheme scheme: String?, host: String? = nil, port: UInt? = nil, path: String, query: [String: AnyObject]? = nil, fragment: String? = nil) -> NSURL? {
+        
         let components = NSURLComponents(path: path, query: query, fragment: fragment)
+        
         components.scheme = scheme
-        components.host = host
-        components.port = port
+        components.host   = host
+        components.port   = port
+        
         return components.URL
     }
     
     @nonobjc
     static func build(scheme scheme: String?, host: String? = nil, port: UInt? = nil, path: [AnyObject]? = nil, query: [String: AnyObject]? = nil, fragment: String? = nil) -> NSURL? {
+        
         let components = NSURLComponents(path: path, query: query, fragment: fragment)
+        
         components.scheme = scheme
-        components.host = host
-        components.port = port
+        components.host   = host
+        components.port   = port
+        
         return components.URL
     }
 }
-
